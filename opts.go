96f819c5aa98db1ce986d086a6de6161b2b99cbd
Pavel Tsayukov <p.tsayukov@gmail.com>
2025-07-03 02:48:55 +0300

    Add WithContext function

--- opts.go
+++ opts.go
@@ -2,3 +2,17 @@
 // Copyright (c) 2025 Pavel Tsayukov p.tsayukov@gmail.com
 
 package rqx
+
+import (
+	"context"
+
+	"github.com/tsayukov/optparams"
+)
+
+// WithContext sets the given [context.Context] for the current request.
+func WithContext(ctx context.Context) optparams.Func[doParams] {
+	return func(params *doParams) error {
+		params.ctx = ctx
+		return nil
+	}
+}
