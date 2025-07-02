4555a5a045413776fa0e68b60f7c4f70b0131538
Pavel Tsayukov <p.tsayukov@gmail.com>
2025-07-03 02:35:48 +0300

    Implement methods for handler struct to apply or match different handlers

--- handler.go
+++ handler.go
@@ -41,3 +41,47 @@ type (
 	// matches one of [RateLimitStatuses].
 	RateLimitHandler func(ctx context.Context, resp *http.Response) error
 )
+
+func (h *handler) applyBefore(req *http.Request) error {
+	for _, fn := range h.beforeResponse {
+		if err := fn(req); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (h *handler) applyAfter(resp *http.Response) error {
+	for _, fn := range h.afterResponse {
+		if err := fn(resp); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (h *handler) matchOK(resp *http.Response) (match bool, _ error) {
+	if h.okResponse == nil {
+		return false, nil
+	}
+
+	result, err := h.okResponse(resp)
+	if result != nil || err != nil {
+		return true, err
+	}
+
+	return false, nil
+}
+
+func (h *handler) matchError(resp *http.Response) error {
+	for _, errorHandler := range h.errorResponses {
+		err := errorHandler(resp)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
